24da921d297248d8682129a2c73353f2ae4e2488
Vinicius Campanholi <viniciuscampanholi.vc@gmail.com>
2023-03-28 08:51:40 -0300

    Exceptions: Solucao boa

--- Exceptions/exceptions/reservationproject/application/Program.java
+++ Exceptions/exceptions/reservationproject/application/Program.java
@@ -6,46 +6,44 @@ import java.util.Date;
 import java.util.Scanner;
 
 import exceptions.reservationproject.application.model.entities.Reservation;
+import exceptions.reservationproject.application.model.exceptions.DomainException;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args){
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
 
-		System.out.println("Room number: ");
-		int room = sc.nextInt();
-
-		System.out.println("Check-in date (dd/MM/yyyy): ");
-		Date checkIn = simpleDateFormat.parse(sc.next());
-
-		System.out.println("Check-out date (dd/MM/yyyy): ");
-		Date checkOut = simpleDateFormat.parse(sc.next());
-
-		if (!checkOut.after(checkIn)) {
-			System.out.println("A data de check-in nao pode ser maior que a data de check-out!");
-		} else {
+		try {
+			System.out.println("Room number: ");
+			int room = sc.nextInt();
+	
+			System.out.println("Check-in date (dd/MM/yyyy): ");
+			Date checkIn = simpleDateFormat.parse(sc.next());
+	
+			System.out.println("Check-out date (dd/MM/yyyy): ");
+			Date checkOut = simpleDateFormat.parse(sc.next());
+	
 			Reservation reservation = new Reservation(room, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation.toString());
-
+	
 			System.out.println();
 			System.out.println("Insira a data para atualizar a reserva: ");
-
+	
 			System.out.println("Check-in date (dd/MM/yyyy): ");
 			checkIn = simpleDateFormat.parse(sc.next());
-
+	
 			System.out.println("Check-out date (dd/MM/yyyy): ");
 			checkOut = simpleDateFormat.parse(sc.next());
-
-			String error = reservation.updateDates(checkIn, checkOut);
-			if(error != null) {
-				System.out.println("Erro ao fazer a reserva: "+error);
-			} else {
-				System.out.println("Reservation: " + reservation);				
-			}
-
+	
+			reservation.updateDates(checkIn, checkOut);
+			System.out.println("Reservation: " + reservation);
+			
+		} catch(ParseException e) {
+			System.out.println("Formato de data invalido");
+		} catch(DomainException e) {
+			System.out.println("Erro: "+ e.getMessage());
 		}
 	}
-
 }
--- Exceptions/exceptions/reservationproject/application/model/entities/Reservation.java
+++ Exceptions/exceptions/reservationproject/application/model/entities/Reservation.java
@@ -4,65 +4,62 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import exceptions.reservationproject.application.model.exceptions.DomainException;
+
 public class Reservation {
 
 	private Integer roomNumber;
 	private Date checkIn;
 	private Date checkOut;
-	
+
 	private static SimpleDateFormat simpleDateFormate = new SimpleDateFormat("dd/MM/yyyy");
-	
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut){
+		if (!checkOut.after(checkIn)) {
+			throw new DomainException("A data de check-in nao pode ser maior que a data de check-out");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
-	
+
 	public Integer getRoomNumber() {
 		return roomNumber;
 	}
-	
+
 	public void setRoomNumber(Integer roomNumber) {
 		this.roomNumber = roomNumber;
 	}
-	
+
 	public Date getCheckIn() {
 		return checkIn;
 	}
-	
+
 	public Date getCheckOut() {
 		return checkOut;
 	}
-	
+
 	public long duration() {
 		long diffMilliseconds = checkOut.getTime() - checkIn.getTime();
 		long diff = TimeUnit.DAYS.convert(diffMilliseconds, TimeUnit.MILLISECONDS);
 		return diff;
 	}
-	
-	public String updateDates(Date checkin, Date checkout) {
+
+	public void updateDates(Date checkin, Date checkout){
 		Date hoje = new Date();
 		if (checkIn.before(hoje) || checkOut.before(hoje)) {
-			return "As datas nao podem ser anteriores a data atual";
+			throw new DomainException("As datas nao podem ser anteriores a data atual");
 		}
 		if (!checkOut.after(checkIn)) {
-			return "A data de check-in nao pode ser maior que a data de check-out!";
+			throw new DomainException("A data de check-in nao pode ser maior que a data de check-out");
 		}
 		this.checkIn = checkin;
 		this.checkOut = checkout;
-		return null;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Room "
-				+ roomNumber
-				+ ", check-in: "
-				+ simpleDateFormate.format(checkIn)
-				+ ", "
-				+ simpleDateFormate.format(checkOut)
-				+ ", "
-				+ duration()
-				+ " nights";
+		return "Room " + roomNumber + ", check-in: " + simpleDateFormate.format(checkIn) + ", "
+				+ simpleDateFormate.format(checkOut) + ", " + duration() + " nights";
 	}
 }
